e0ca93bf7668763bdc4d20e6557330be240c4128
rolikun <roland.kun98@gmail.com>
2023-01-07 20:16:07 +0100

    fix(todos-container-style.ts):fixed flex style

--- components/todos-container/todos-container-style.ts
+++ components/todos-container/todos-container-style.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 export const todosContainerStyles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
     marginTop: 16,
     width: "100%",
     alignItems: "center",
