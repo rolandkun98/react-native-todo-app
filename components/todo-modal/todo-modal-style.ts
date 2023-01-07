5633197904e4bf6291c03a387307e71e0d4e1049
rolikun <roland.kun98@gmail.com>
2023-01-07 20:14:04 +0100

    feat(todo-modal-style.ts):add landscape styles

--- components/todo-modal/todo-modal-style.ts
+++ components/todo-modal/todo-modal-style.ts
@@ -1,26 +1,41 @@
 import { StyleSheet } from "react-native";
 import { colors, fontFamilies } from "../../utils/style-vars";
 
-export const todoModalStyles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-    alignItems: "center",
-    height: "100%",
-    backgroundColor: colors.tertiary,
-  },
-  textInput: {
-    height: 50,
-    width: "80%",
-    backgroundColor: colors.light,
-    marginBottom: 16,
-    borderRadius: 8,
-    paddingLeft: 16,
-    paddingRight: 16,
-    fontFamily: fontFamilies.primary,
-    letterSpacing: 1,
-  },
-  buttonContainer: {
-    height: 110,
-    justifyContent: "space-between",
-  },
-});
+export const todoModalStyles = (orientation: "portrait" | "landscape") => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      paddingTop: orientation === "portrait" ? 50 : 0,
+      alignItems: orientation === "portrait" ? "center" : "center",
+      justifyContent: orientation === "portrait" ? "flex-start" : "center",
+      backgroundColor: colors.tertiary,
+      flexDirection: orientation === "portrait" ? "column" : "row",
+    },
+    headerContainer: {
+      flex: orientation === "portrait" ? 0 : 1,
+      width: orientation === "portrait" ? "100%" : "50%",
+      height: orientation === "portrait" ? "auto" : 130,
+      alignItems: "center",
+      justifyContent:
+        orientation === "portrait" ? "flex-start" : "space-between",
+    },
+    textInput: {
+      height: 50,
+      width: "80%",
+      backgroundColor: colors.light,
+      marginBottom: orientation === "portrait" ? 16 : 0,
+      borderRadius: 8,
+      paddingLeft: 16,
+      paddingRight: 16,
+      fontFamily: fontFamilies.primary,
+      letterSpacing: 1,
+    },
+    buttonContainer: {
+      flex: orientation === "portrait" ? 0 : 1,
+      height: orientation === "portrait" ? 110 : 130,
+      justifyContent: "space-between",
+      alignItems: orientation === "portrait" ? "flex-start" : "center",
+    },
+  });
+};
